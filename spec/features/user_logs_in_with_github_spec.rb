66f033d497e941bb0b771e667c017c7bd4aa95e8
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-22 08:06:49 -0600

    login of existing user test passes

---
+++ spec/features/user_logs_in_with_github_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "user logs in" do
+  scenario "using github oauth" do
+    stub_omniauth
+
+    visit root_path
+
+    expect(page).to have_link("Login")
+
+    click_link "Login"
+
+    expect(page).to have_content("Hello")
+    expect(page).to have_content("Matt")
+  end
+
+  def stub_omniauth
+    OmniAuth.config.test_mode = true
+
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      provider: "github",
+      uid: "12345678",
+      info: {
+        email: "matthew.devoe@gmail.com",
+        name: "Matt",
+        nickname: "Matt",
+
+      },
+      credentials: {
+        token: "12345abcdefghijklmnop"
+      }
+      })
+  end
+end
