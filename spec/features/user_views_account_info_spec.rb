531f6f4036d3dfd235737c21cbc0a29e15db27c3
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-23 11:29:02 -0600

    vcr and webmock rspec added to gem file, configured and added to current tests

--- spec/features/user_views_account_info_spec.rb
+++ spec/features/user_views_account_info_spec.rb
@@ -5,11 +5,13 @@ feature "user sees account info" do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit dashboard_index_path
+    VCR.use_cassette("features/user_views_account_info_spec.rb") do
+      visit dashboard_index_path
 
-    expect(page).to have_content("#{user.nickname}")
-    expect(page).to have_content("Followers: 1")
-    expect(page).to have_content("Following: 1")
-    expect(page).to have_content("Starred Repos: 3")
+      expect(page).to have_content("#{user.nickname}")
+      expect(page).to have_content("Followers: 2")
+      expect(page).to have_content("Following: 1")
+      expect(page).to have_content("Starred Repos: 3")
+    end
   end
 end
