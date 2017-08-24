43ece588383a1993535985b4ee700758986bdd5c
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-24 09:20:21 -0600

    commit controller, index action and index view added, initial test passes

---
+++ app/controllers/commit_controller.rb
@@ -0,0 +1,7 @@
+class CommitController < ApplicationController
+
+  def index
+
+  end
+
+end
--- spec/features/user_views_list_of_recent_commits_spec.rb
+++ spec/features/user_views_list_of_recent_commits_spec.rb
@@ -7,8 +7,8 @@ feature 'user sees' do
 
     VCR.use_cassette("features/user_views_list_of_recent_commits_spec.rb") do
       visit commit_index_path
-
-      expect(page).to have_content("enter commit here")
-    end  
+    save_and_open_page
+      expect(page).to have_content("#{user.nickname}'s Recent Commits")
+    end
   end
 end
