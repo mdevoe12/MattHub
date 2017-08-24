e496777d33fe25b4ab4054ce04852fa225ca9a87
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-24 11:54:45 -0600

    created test

---
+++ spec/features/user_views_list_of_orgs_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'user sees' do
+  scenario 'list of the organizations they belong to' do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    VCR.use_cassette("features/user_views_list_of_orgs_spec.rb") do
+      visit orgs_path
+
+      expect(page).to have_content("#{user.username}'s Organizations")
+
+    end
+  end
+end
